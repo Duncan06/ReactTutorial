665242417984a463c5e076ecc1bdf1909ed3a261
Duncan06 <tylerdownie0@gmail.com>
2021-09-26 00:21:49 -0700

    Add firebase api for data storage

--- code/react-vs-vanilla-js-example/meetup-react/src/pages/NewMeetup.js
+++ code/react-vs-vanilla-js-example/meetup-react/src/pages/NewMeetup.js
@@ -1,10 +1,22 @@
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupPage() {
+  function AddMeetupHandler(meetupData) {
+    fetch(
+      "https://react-meetup-demo-5ecdc-default-rtdb.firebaseio.com/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupData),
+        headers: {
+          "Content-type": "application/json",
+        },
+      }
+    );
+  }
   return (
     <section>
       <h1> New Meetup Page</h1>
-      <NewMeetupForm />
+      <NewMeetupForm onAddMeetup={AddMeetupHandler} />
     </section>
   );
 }
