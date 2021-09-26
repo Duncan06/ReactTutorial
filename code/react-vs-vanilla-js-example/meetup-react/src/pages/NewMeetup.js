0a7f028f082fd602f9802fa53183a565b44710a5
Duncan06 <tylerdownie0@gmail.com>
2021-09-26 00:57:43 -0700

    Add useHistory to reroute after form submisison.

--- code/react-vs-vanilla-js-example/meetup-react/src/pages/NewMeetup.js
+++ code/react-vs-vanilla-js-example/meetup-react/src/pages/NewMeetup.js
@@ -1,6 +1,10 @@
+import { useHistory } from "react-router-dom";
+
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupPage() {
+  const history = useHistory();
+
   function AddMeetupHandler(meetupData) {
     fetch(
       "https://react-meetup-demo-5ecdc-default-rtdb.firebaseio.com/meetups.json",
@@ -11,7 +15,9 @@ function NewMeetupPage() {
           "Content-type": "application/json",
         },
       }
-    );
+    ).then(() => {
+      history.replace("/");
+    });
   }
   return (
     <section>
